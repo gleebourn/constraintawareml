e9b88ba1bb134077e0c61d93e7593210b1ab6075
George <georgelee@uvic.ca>
2024-07-19 05:53:35 -0700

    Add sequential model generation and Fbeta metric

--- consawml.py
+++ consawml.py
@@ -28,3 +28,48 @@ def preproc_bin_class(df,seed,label_col='label',label_class='Malicious',
   #Returns X_train, X_test, y_train, y_test
   return train_test_split(X,y,test_size,random_state=seed,stratify=y)
   
+class FbetaMetric(Metric):
+  def __init__(self, beta=1, threshold=0.5, **kwargs):
+    super(FbetaMetric, self).__init__(**kwargs)
+    self.beta = beta
+    self.threshold = threshold
+    self.tp = self.add_weight(name='true_positives', initializer='zeros')
+    self.fp = self.add_weight(name='false_positives', initializer='zeros')
+    self.fn = self.add_weight(name='false_negatives', initializer='zeros')
+
+  def update_state(self, y_true, y_pred, sample_weight=None):
+    y_pred_binary = cast(greater_equal(y_pred, self.threshold),float32)
+    #self.tp.assign_add(reduce_sum(cast(y_true,float32) * y_pred_binary))
+    y_true_float=cast(y_true,float32)
+    self.tp.assign_add(reduce_sum(y_true_float * y_pred_binary))
+    self.fp.assign_add(reduce_sum((1 - y_true_float) * y_pred_binary))
+    self.fn.assign_add(reduce_sum(y_true_float * (1 - y_pred_binary)))
+
+  def result(self):
+    precision = self.tp / (self.tp + self.fp + epsilon())
+    recall = self.tp / (self.tp + self.fn + epsilon())
+    fbeta = (1 + self.beta ** 2) * (precision * recall)/\
+           ((self.beta ** 2 * precision) + recall + epsilon())
+    return fbeta
+
+
+def mk_two_layer_perceptron(df,loss,l1_size=128,l2_size=32,optimizer='adam',
+                            #metrics=['precision', 'recall'],#, 'fscore'],
+                            metrics=['accuracy','binary_accuracy',
+                                     FbetaMetric()],#, 'fscore'],
+                            activation=LeakyReLU(alpha=0.01)):
+
+  m=Sequential([Input(shape=df.columns.shape),#,activation=activation),
+                BatchNormalization(),Dense(l1_size,activation=activation),
+                BatchNormalization(),Dense(l2_size,activation=activation),
+                #Just use sigmoid on last step,nicer
+                Dense(1,activation='sigmoid')])
+  m.compile(optimizer=optimizer, loss=loss, metrics=metrics)
+
+  return m
+
+def fit_model(m,training_features,training_labels,
+              epochs=200,batch_size=32):
+  m.fit(training_features,training_labels,
+        epochs=epochs,batch_size=batch_size)
+
