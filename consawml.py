fd80efd493a8d5cd45ba6595d1a693683002dabb
George <georgelee@uvic.ca>
2024-07-19 09:59:55 -0700

    More hiding things away with functions

--- consawml.py
+++ consawml.py
@@ -15,6 +15,24 @@ from tensorflow.math import greater_equal
 def preproc_bin_class(df,seed,label_col='label',label_class='Malicious',
                       numeric_only=True,test_size=.3,
                       non_feature_cols=['id','ini_timestamp','label','detailed_label']):
+  '''
+  Pick out relevant columns from a dataframe and split it into training and test data
+
+  Parameters:
+    df: Dataframe or is a string treated as the location of a pickled DataFrame
+    seed: Random seed for train-test random splitting purposes
+    label_col: The name of the column corresponding to the classification problem
+    label_class: The value in label_col corresponding to the class of interest
+    numeric_only: Ignore categoricL features
+    test_size: The proportion of the data to use for testing
+    non_feature_cols: List of columns to not use as features
+  
+  Returns:
+    X_train: Features for training
+    X_test: Features for testing
+    y_train: Labels for training
+    y_test: Labels for testing
+  '''
   if isinstance(df,str):
     df = read_pickle(df)
 
@@ -53,13 +71,21 @@ class FbetaMetric(Metric):
     return fbeta
 
 
-def mk_two_layer_perceptron(df,loss,l1_size=128,l2_size=32,optimizer='adam',
-                            #metrics=['precision', 'recall'],#, 'fscore'],
-                            metrics=['accuracy','binary_accuracy',
-                                     FbetaMetric()],#, 'fscore'],
+def mk_two_layer_perceptron(X,loss,l1_size=128,l2_size=32,optimizer='adam',
+                            metrics=['accuracy','binary_accuracy',FbetaMetric()],
                             activation=LeakyReLU(alpha=0.01)):
+  '''
+  Generate the two layer perceptron that we train using loss functions of interest
 
-  m=Sequential([Input(shape=df.columns.shape),#,activation=activation),
+  Parameters:
+    X: A DataFrame of features - used to figure out the input layer size
+    loss: The loss function to train the model on
+
+  Returns:
+    m: A model to be trained by fit_model
+  '''
+
+  m=Sequential([Input(shape=X.columns.shape),#,activation=activation),
                 BatchNormalization(),Dense(l1_size,activation=activation),
                 BatchNormalization(),Dense(l2_size,activation=activation),
                 #Just use sigmoid on last step,nicer
@@ -68,10 +94,29 @@ def mk_two_layer_perceptron(df,loss,l1_size=128,l2_size=32,optimizer='adam',
 
   return m
 
+#The following are kind of redundant as things stand,
+#but may need to add more complexity for testing certain
+#approaches to dealing with unbalanced classification
 def fit_model(m,training_features,training_labels,
               epochs=200,batch_size=32):
   m.fit(training_features,training_labels,
         epochs=epochs,batch_size=batch_size)
 
 def evaluate_model(m,testing_features,testing_labels):
+  '''
+  Test and score a trained model using the test data
+
+  Parameters:
+    m: The model to be trained
+    testing_features: The features to be queried
+    testing_labels: The correct classifications of the features
+  '''
+  m.evaluate(testing_features,testing_labels)
+
+def evaluate_metric(df,seed,loss='binary_crossentropy',
+                    epochs=200,batch_size=32):
+  X_train,X_test,y_train,y_test = preproc_bin_class(df,seed)
+  m=mk_two_layer_perceptron(X_train,loss)
+  m.fit(training_features,training_labels,
+        epochs=epochs,batch_size=batch_size)
   m.evaluate(testing_features,testing_labels)
