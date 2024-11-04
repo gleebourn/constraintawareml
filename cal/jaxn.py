80aa297b8e16d7f840151bf8ca8cca7e797b4cb4
George Lee <glee@bournemouth.ac.uk>
2024-11-04 15:47:14 +0000

    Add example jupyter notebook for jax work

--- cal/jaxn.py
+++ cal/jaxn.py
@@ -1,7 +1,12 @@
 from jax.nn import sigmoid,relu
-from jax.numpy import dot,vectorize,zeros,sum as jsum
-from jax.random import normal,key
+from jax.numpy import dot,vectorize,zeros,square,sqrt,sum as jsum
+from jax.random import normal,key,randint
 from jax import grad,jit
+
+#@jit #jitting this causes an iteration over 0d array error!!!!
+def rand_batch(X,y,batch_size,key):
+  indices=randint(key,batch_size,0,y.shape[0])
+  return X[indices],y[indices]
 
 @jit
 def f(x,params):
@@ -26,23 +31,61 @@ def smooth_f_beta_loss(y_pred,y,lamb=.5):
   return weighted_false/(tp+weighted_false)
 
 @jit
+def smooth_quotient_loss(y_pred,y,lamb=.5):
+  tp=jsum(y*y_pred)
+  fp=jsum((1-y)*y_pred)
+  fn=jsum(y*(1-y_pred))
+  weighted_false=(lamb*fp+(1-lamb)*fn)
+  return weighted_false/tp
+
+@jit
 def loss_batch(X,y,params):
   y_pred=f_v(X,params)
-  return smooth_f_beta_loss(y_pred,y)
+  #return smooth_f_beta_loss(y_pred,y)
+  return smooth_quotient_loss(y_pred,y)
   
 d_params=grad(loss_batch,argnums=2)
 
+def hlp_init_params(input_dim,l_dim):
+  return dict(A1=zeros(shape=(l_dim,input_dim)),
+              b1=zeros(shape=l_dim),
+              A2=zeros(shape=l_dim),
+              b2=0.)
+
 class hidden_layer_perceptron:
 
-  def __init__(self,input_dim,l_dim,seed=0):
+  def __init__(self,input_dim,l_dim,lr=.1,seed=0):
     self.key=key(seed)
+    self.lr=lr
     self.input_dim=input_dim
     self.l_dim=l_dim
-    self.params=dict(A1=zeros(shape=(l_dim,input_dim)),
-                     b1=zeros(shape=l_dim),
-                     A2=zeros(shape=l_dim),
-                     b2=0.)
+    self.params=hlp_init_params(input_dim,l_dim)
     self.randomise_params()
+
+  def init_adam(self,beta1=.9,beta2=.999,lr=.01):
+    #Don't bother with the fairly pointless time dependent bias removal
+    #Eg, for default beta2 beta2^t<.01 when t>4600 or so.
+    self.beta1=beta1
+    self.beta2=beta2
+    self.gamma1=1-beta1
+    self.gamma2=1-beta2
+    self.lr=lr
+    self.m=hlp_init_params(self.input_dim,self.l_dim)
+    self.v=0
+
+  def adam_step(self,X,y,mk_batch=False,eps=.00000001):
+    if mk_batch:
+      X,y=rand_batch(X,y,mk_batch,self.key)
+    upd=d_params(X,y,self.params)
+    self.v*=self.beta2
+    for key in upd:
+      self.m[key]=self.beta1*self.m[key]+self.gamma1*upd[key]
+      self.v+=self.gamma2*jsum(square(upd[key]))
+
+    mult=self.lr/(eps+sqrt(self.v))
+    for key in upd:
+      self.params[key]-=mult*self.m[key]
+
 
   def randomise_params(self,amount=1):
     self.params['A1']*=1-amount
@@ -53,7 +96,6 @@ class hidden_layer_perceptron:
     self.params['b1']+=amount*normal(self.key,shape=self.l_dim)
     self.params['A2']+=amount*normal(self.key,shape=self.l_dim)
     self.params['b2']+=amount*normal(self.key)
-    
 
   def infer(self,x):
     return f(x,self.params)
@@ -64,19 +106,21 @@ class hidden_layer_perceptron:
   def loss_batch(self,X,y):
     return loss_batch(X,y,self.params)
 
-  def gradient_desc_step(self,X,y,lr,noise=0):
-    score=loss_batch(X,y,self.params)
+  def gradient_desc_step(self,X,y,noise=0,mk_batch=False):
+    if mk_batch:
+      X,y=rand_batch(X,y,mk_batch,self.key)
     upd=d_params(X,y,self.params)
-    self.params['A1']-=lr*upd['A1']
-    self.params['b1']-=lr*upd['b1']
-    self.params['A2']-=lr*upd['A2']
-    self.params['b2']-=lr*upd['b2']
-    if noise: self.randomise_params(amount=score*lr*noise)
+    self.params['A1']-=self.lr*upd['A1']
+    self.params['b1']-=self.lr*upd['b1']
+    self.params['A2']-=self.lr*upd['A2']
+    self.params['b2']-=self.lr*upd['b2']
+    if noise:
+      score=loss_batch(X,y,self.params)
+      self.randomise_params(amount=score*self.lr*noise)
 
   def repeated_desc_step(self,X,y,lr,n_runs=1000,noise=.01,verbose=False):
     for i in range(n_runs):
       self.gradient_desc_step(X,y,lr,noise=noise)
       if verbose and not(i%100):
         print('Loss(X,y)=',self.loss_batch(X,y))
-
 
